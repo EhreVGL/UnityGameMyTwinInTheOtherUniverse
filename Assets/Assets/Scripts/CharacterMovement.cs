b01f7bb7bc796f02ef6fac9a7a6356ed4345c6c0
EhreVGL <altunisik.onur.97@gmail.com>
2022-07-06 13:41:08 +0300

    Add CharacterMovement Script and Start UI

---
+++ Assets/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class CharacterMovement : MonoBehaviour
+{
+    // UI OBJECTS
+    [SerializeField] private TextMeshProUGUI startText;
+    [SerializeField] private Image startLayer;
+    
+    // CAMERA OBJECTS
+    [SerializeField] private Camera mainCamera;
+    private Vector3 cameraPosition;
+
+    private Transform transform;
+    private Vector3 constantMovement;
+    private float constantMovementMultiplier;
+    private Vector3 verticalMovement;
+    private float verticalMovementMultiplier;
+
+
+    private Touch touch;
+    private Vector2 touchBeganPosition;
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+        constantMovement = Vector3.forward;
+        constantMovementMultiplier = 8f;
+        verticalMovement = Vector3.right;
+        verticalMovementMultiplier = 3f;
+        touchBeganPosition = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPosition = new Vector3(mainCamera.transform.position.x - transform.position.x, mainCamera.transform.position.y - transform.position.y, mainCamera.transform.position.z - transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        mainCamera.transform.position = transform.position + cameraPosition;
+        if (Input.touchCount > 0)
+        {
+            //Disable start UI
+            startText.enabled = false;
+            startLayer.enabled = false;
+
+            touch = Input.GetTouch(0);
+
+            //Movement z-axis
+            transform.position += constantMovement * (constantMovementMultiplier * Time.fixedDeltaTime);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchBeganPosition = touch.position;
+            }
+
+            if (touchBeganPosition.x - touch.position.x > 30)
+            {
+                transform.position -= verticalMovement * (verticalMovementMultiplier * Time.fixedDeltaTime);
+            }
+            else if (touchBeganPosition.x - touch.position.x < -30)
+            {
+                transform.position += verticalMovement * (verticalMovementMultiplier * Time.fixedDeltaTime);
+            }
+
+        }
+    }
+}
